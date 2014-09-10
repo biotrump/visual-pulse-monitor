9c2195c8c99b5b73263184875a6616261f153082
biotrump <biotrump@users.noreply.github.com>
2014-09-10 16:48:49 +0800

    Update bandlimit.m
    he larger the index, the larger the frequency.

--- code/bandlimit.m
+++ code/bandlimit.m
@@ -1,4 +1,6 @@
 function [pass_pows pass_freq] = bandlimit(pows, freq, low, high)
+  %pows, freq are all sorted/ordered by the freq along the index axis.
+  % The larger the index, the larger the frequency.
   % select a certain frequency range from the power spectrum of a signal
   % find the upper and lower bound
   low_limit = min(find(freq > low));  % find the elements of freq, 1-d array, whose element is greater than low.
